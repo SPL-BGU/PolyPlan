a4987850f7d25f14e2c676b8562d3a4a10fde82e
Yarin Bnyamin <bnyamin@post.bgu.ac.il>
2023-08-04 16:10:00 +0300

    fixed action selection at start state bug

--- agents/best_first_search.py
+++ agents/best_first_search.py
@@ -1,90 +1,94 @@
 from agents.polycraft_agent import PolycraftAgent
 import networkx as nx
 from random import choice
-import random
 
 
 class BFS(PolycraftAgent):
-    """Online Best-First Search agent with mask for invalid actions"""
+    """
+    Online Best-First Search agent with mask for invalid actions
+
+    :all_successors: set of all possible successors
+    :frontier: list of the current successors
+    :node: tuple of (state, action)
+    """
 
     def __init__(self, env):
         super().__init__(env)
         self.action_space = self.env.action_space.n
         self.graph = nx.DiGraph()
-
-        self.frontier = set()
-        self.current_successors = []
-        self.visited = set()
-
-        # init graph
         self.start_state = str(env.state)
 
-        # for update useless actions
-        self.last_state = (None, 0)
-        self.last_action = None
+        self.all_successors = set()
+        self.frontier = []
+        self.visited = set()
+        self.last_node = "Start"
 
-        # for update useless states
-        self.pre_state = None
-        self.pre_action = None
-
-    def priorite_calculation(self, state_action: tuple) -> int:
-        """calculate the priority of state and action, higher is better
-        state_action: tuple of (state, action)"""
+    def priorite_calculation(self, node: tuple) -> int:
+        """
+        calculate the priority of state and action, higher is better
+        :node: tuple of (state, action)
+        """
         return 0
 
-    def top_priority(self, all_set: set) -> list:
-        """return a list of the top priority values from a set"""
+    def top_priority(self, node_set: set) -> list:
+        """
+        return a list of the top priority values from a set
+        :node_set: set of nodes (tuples of (state, action))
+        """
         top_list = []
 
         max_priority = 0
-        for state_action in all_set:
-            priority = self.priorite_calculation(state_action)
+        for node in node_set:
+            priority = self.priorite_calculation(node)
             if priority < max_priority:
                 continue
             elif priority > max_priority:
                 max_priority = priority
-            top_list.append((priority, state_action))
+            top_list.append((priority, node))
 
-        top_list = [tup for priority, tup in top_list if priority == max_priority]
+        top_list = [node for priority, node in top_list if priority == max_priority]
         return top_list
 
     def choosing(self, top_list: list) -> tuple:
-        """return a tuple of (state, action)"""
+        """select a node (state, action) from the top priority list"""
         return choice(top_list)
 
     def choose_action(self, state=None) -> int:
-        """choose random action from the top priority actions"""
+        """choose the next action to perform"""
         state = str(state)
         self.check_state_exist(state)
 
-        if state == self.start_state:  # explore from frontier
-            frontier = self.top_priority(set(self.graph.neighbors(state)))
-            state_action = self.choosing(frontier)
-            path_to_state = self.path_to_state(state_action[0])
-            self.walk_in_path(path_to_state)
-        else:  # explore from near neighbors
-            if len(self.current_successors) == 0:
-                self.last_state = (None, 0)
+        if state == self.start_state:  # explore from all_successors
+            frontier = self.top_priority(self.all_successors)
+            node = self.choosing(frontier)
+            self.get_to_state(node[0])
+        else:  # explore from frontier
+            if len(self.frontier) == 0:
+                self.last_node = (None, 0)
                 return 0
-            state_action = self.choosing(self.current_successors)
+            node = self.choosing(self.frontier)
 
-        self.frontier.discard(state_action)
-        self.last_state = state_action
-        return state_action[1]
+        self.all_successors.discard(node)
+        self.last_node = node
+        return node[1]
 
     def check_state_exist(self, state):
+        """
+        add state to the graph if it doesn't exist
+        if it does exist make a mask for invalid actions
+        """
         if state not in self.visited:
             self.visited.add(state)
-            self.graph.add_edge(self.last_state, state)
+            self.graph.add_edge(self.last_node, state)
             for i in range(self.action_space):
-                next_state = (state, i)
-                self.graph.add_edge(state, next_state)
-                self.frontier.add(next_state)
-        elif self.last_state[0] == state:  # we got back to the same state
+                node = (state, i)
+                self.graph.add_edge(state, node)
+                self.all_successors.add(node)
+        elif self.last_node[0] == state:  # we got back to the same state
             # invalid action masking
-            self.graph.remove_node(self.last_state)
+            self.graph.remove_node(self.last_node)
 
-        self.current_successors = self.top_priority(set(self.graph.neighbors(state)))
+        self.frontier = self.top_priority(set(self.graph.neighbors(state)))
 
     def path_to_state(self, state):
         try:
@@ -93,6 +97,10 @@ class BFS(PolycraftAgent):
             return None
 
     def walk_in_path(self, path):
-        for tup in path:
-            if type(tup) == tuple:
-                self.env.step(tup[1])
+        for node in path:
+            if type(node) == tuple:
+                self.env.step(node[1])
+
+    def get_to_state(self, state):
+        path = self.path_to_state(state)
+        self.walk_in_path(path)
